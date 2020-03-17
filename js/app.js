27cb7c034a3c2ae66c8d41adea36a6821c4080bf
vanduc2514 <vanduc2514@gmail.com>
2020-03-17 10:40:41 +0700

    Sửa lại thuật toán

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 function showSpecialNum() {
     let count = 1;
     let numbers = +document.getElementById('num-input').value;
-    let result = '';
-    let index = 1;
+    let result = [];
+    let index = 2;
     for (index; count <= numbers;) {
         if (checkPrime(index)) {
-            result = result + index + ", ";
+            result.push(index);
             count++;
         }
         index++;
     }
-    document.getElementById('num-display').innerHTML = result;
+    document.getElementById('num-display').innerHTML = result.join(", ");
 }
 
 function checkPrime(number) {
-    for (let i = 2; i <= number/2; i++) {
+    for (let i = 2; i <= number/Math.sqrt(number); i++) {
         if (number % i === 0) {
             return false;
         }
